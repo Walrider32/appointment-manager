0bfef59cf15c3f7b6d2bfd52d99a575a674d0502
SSzabolcs-glitch <sszabolcs2001@gmail.com>
2023-11-25 15:20:00 +0100

    Weekly, odd-weeks, even-weeks working on 2 appointments

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -17,66 +17,88 @@ class CalendarController extends Controller
 
     public function getCalendarAppointments(Request $request)
     {
-        if($request->ajax()) {
-            $start = $request->input('start');
-            $end = $request->input('end');
+        // Get the start and end parameters from the AJAX request
+        $start = $request->input('start');
+        $end = $request->input('end');
 
-            $appointments = Appointment::where(function ($query) use ($start, $end) {
-            // Include appointments that overlap with the requested time range
+        // Query the database for appointments that overlap with the requested time range
+        $appointments = Appointment::where(function ($query) use ($start, $end) {
             $query->whereBetween('date_start', [$start, $end])
                 ->orWhereBetween('date_end', [$start, $end])
                 ->orWhere(function ($q) use ($start, $end) {
                     $q->where('date_start', '<=', $start)
                         ->where('date_end', '>=', $end);
-            });
+                });
         })->get();
 
+        // Prepare an array to store the formatted events
         $events = [];
 
+        // Process each fetched appointment
         foreach ($appointments as $appointment) {
-            // Handle recurring appointments
+            // Handle recurring appointments and get their occurrences
             $occurrences = $this->getAppointmentOccurrences($appointment);
 
+            // Format each occurrence and add it to the events array
             foreach ($occurrences as $occurrence) {
                 $events[] = [
                     'title' => $appointment->name,
                     'start' => $occurrence['start'],
                     'end' => $occurrence['end'],
+                    'daysOfWeek' => $appointment['day_of_week'],
                     // Add other relevant appointment data here
                 ];
             }
         }
-
+        // Return the formatted events as JSON
         return response()->json($events);
+    }
+
+    // Calculate recurring occurrences for a given appointment
+    private function getAppointmentOccurrences($appointment)
+    {
+    // Initialize an array to store occurrences
+    $occurrences = [];
+
+    // Use Carbon to manipulate dates
+    $currentDate = Carbon::parse($appointment->date_start);
+
+    // Set the end date based on the appointment's recurrence type
+    $endDate = $appointment->date_end ? Carbon::parse($appointment->date_end) : null;
+
+    // Loop until the end date is reached (or a reasonable limit to avoid infinite loops)
+    $loopCount = 0;
+    $maxLoops = 1000; // Adjust as needed to prevent infinite loops
+    while (!$endDate || $currentDate->lte($endDate)) {
+        // Check if the current day is in the recurring days of the week
+        if (
+            ($appointment->recurring === 'none' && $currentDate->isSameDay(Carbon::parse($appointment->date_start))) ||
+            ($appointment->recurring === 'weekly' && in_array($currentDate->dayOfWeek, $appointment->day_of_week)) ||
+            ($appointment->recurring === 'even_weeks' && $currentDate->week % 2 === 0 && in_array($currentDate->dayOfWeek, $appointment->day_of_week)) ||
+            ($appointment->recurring === 'odd_weeks' && $currentDate->week % 2 !== 0 && in_array($currentDate->dayOfWeek, $appointment->day_of_week))
+        ) {
+            // Calculate start and end times for the occurrence
+            $start = Carbon::parse($currentDate->toDateString() . ' ' . $appointment->time_start);
+            $end = Carbon::parse($currentDate->toDateString() . ' ' . $appointment->time_end);
+
+            // Add the occurrence to the array
+            $occurrences[] = [
+                'start' => $start->toDateTimeString(),
+                'end' => $end->toDateTimeString(),
+            ];
+        }
+
+        // Move to the next week for weekly recurring appointments
+        $currentDate->addWeek();
+        $loopCount++;
+
+        // Break the loop if it runs for too long to avoid infinite loops
+        if ($loopCount >= $maxLoops) {
+            break;
         }
     }
 
-    private function getAppointmentOccurrences($appointment)
-    {
-        // Logic to calculate recurring occurrences based on the appointment data
-        // You can use Carbon library to manipulate dates
-
-        $occurrences = [];
-
-        // Example logic, customize according to your needs
-        $currentDate = Carbon::parse($appointment->date_start);
-
-        while ($currentDate->lte(Carbon::parse($appointment->date_end))) {
-            // Check if the current day is in the recurring days of the week
-            if (in_array($currentDate->dayOfWeek, $appointment->day_of_week)) {
-                $start = Carbon::parse($currentDate->toDateString() . ' ' . $appointment->time_start);
-                $end = Carbon::parse($currentDate->toDateString() . ' ' . $appointment->time_end);
-
-                $occurrences[] = [
-                    'start' => $start->toDateTimeString(),
-                    'end' => $end->toDateTimeString(),
-                ];
-            }
-
-            // Move to the next week for weekly recurring appointments
-            $currentDate->addWeek();
-        }
-
-        return $occurrences;
+    // Return the array of occurrences
+    return $occurrences;
     }
 }
--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -6,8 +6,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>
 </head>
@@ -37,10 +37,19 @@ $(document).ready(function () {
             center: 'title',
             right: 'month,agendaWeek,agendaDay'
         },
-        events: '/calendar/appointments',
-        eventRender: function (event, element, view) {
-            event.allDay = event.allDay === 'true';
-        },
+        events: {
+            url: '/calendar/appointments',
+            type: 'GET',
+            data: {
+                // Additional parameters, if needed
+            },
+            success: function (events) {
+                console.log(events);
+            },
+            error: function (err) {
+                console.log('Error fetching events:', err);
+            }
+        }
     })
 });
 </script>
